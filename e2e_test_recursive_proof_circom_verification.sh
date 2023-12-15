2677186764b42ca5d952e53f076a0d793bd2a44b
quan.li <quan.li@okg.com>
2023-12-15 17:29:24 +0800

    update sh

--- e2e_test_recursive_proof_circom_verification.sh
+++ e2e_test_recursive_proof_circom_verification.sh
@@ -1,7 +1,9 @@
 echo "****GENERATING RECURSIVE PLONKY2 PROOF****"
-cargo test -r --color=always --package plonky2_circom_verifier --lib verifier::tests::test_recursive_verifier --no-fail-fast -- -Z unstable-options --show-output
+cargo test -r --color=always --package plonky2_circom_verifier --lib position::tests::test_resursive_single_proof_to_circom --no-fail-fast -- -Z unstable-options --show-output
 echo "DONE ($((end - start))s)"
 cd circom/e2e_tests && ./run.sh && cd .. && cd ..
+
+
 echo "****GENERATING A NEW RECURSIVE PLONKY2 PROOF****"
 cargo test -r --color=always --package plonky2_circom_verifier --lib verifier::tests::test_recursive_verifier --no-fail-fast -- -Z unstable-options --show-output
 cd circom/e2e_tests && ./run2.sh && cd .. && cd ..
